307321e8f03ff1143b06fb3e2ef0079007aa10af
LeandraCodes821 <Ms.knowitall2336@gmail.com>
2022-11-16 13:56:59 -0800

    Removes files from source folder

---
+++ src/Components/About.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+function About() {
+    return (
+        <div>
+            <section className="section--about">
+                <h2 className="about">About</h2>
+                <p className="about--text">
+                    I am a Frontend Developer with an interest in creating user
+                    friendly websites. My skills currently include Github, Postman,
+                    APIs, JavaScript, React.js, HTML5, CSS3, Bootstrap, Node.js Visual
+                    Studio Code, Express, Morgan and Axios. I am always trying to keep
+                    myself aware of current best practices in tech.
+                </p>
+            </section>
+        </div>
+    )
+}
