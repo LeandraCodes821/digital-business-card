7d5ccad52400522dfeca4dc44218aae5cc8d11b2
LeandraCodes821 <Ms.knowitall2336@gmail.com>
2022-11-16 15:10:28 -0800

    Adds import Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from "react"
-import About from "./Components/About"
+import React from "react";
+import About from "./Components/About";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
       <div>
         <About />
+        <Footer />
       </div>
   );
 }
